831479d6c816210480aa4fc23a15c76b196bcc38
MATHEUS PANTOJA <matheus.pantoja97@gmail.com>
2019-04-09 11:14:44 -0300

    .

--- deamon.py
+++ deamon.py
@@ -1,21 +1,21 @@
 import os
 import time
 import subprocess
+import datetime
 
 def checkServiceALive(serviceName):
         cmd = "netstat -putan | grep "+serviceName+" | wc -l"
         while(True):
-                os.system("echo fetching for "+serviceName+"... >> "+serviceName+".log")
+                os.system("echo "+datetime.datetime.now()+"fetching for "+serviceName+"... >> /root/"+serviceName+".log")
                 output = subprocess.check_output(cmd, shell=True)
 
                 if(int(output) == 6):
-                        os.system("echo "+serviceName +", OK! >>"+serviceName+".log")
+                        os.system("echo "+datetime.datetime.now()+" "+serviceName +", OK! >> /root/"+serviceName+".log")
                 else:
-                        os.system("echo "+serviceName +", service has problems! >> "+serviceName+".log")
-                        os.system("echo restarting, "+ serviceName+">> "+serviceName+".log")
+                        os.system("echo "+datetime.datetime.now()+" "+serviceName +", service has problems! >> /root/"+serviceName+".log")
+                        os.system("echo "+datetime.datetime.now()+" restarting, "+ serviceName+">> /root/"+serviceName+".log")
                         os.system("systemctl restart "+serviceName)
 
                 time.sleep(10)
-
 
 checkServiceALive('nginx')
